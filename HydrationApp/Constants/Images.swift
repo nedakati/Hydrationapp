30b5c6a8d9665ba7b7afb3f56a3f0693dce91d58
Katalin Neda <katalin.neda@halcyonmobile.com>
2022-01-25 19:10:47 +0200

    Setup project and add tab bar

---
+++ HydrationApp/Constants/Images.swift
@@ -0,0 +1,13 @@
+//
+//  Images.swift
+//  HydrationApp
+//
+//  Created by Katalin Neda on 25.01.2022.
+//
+
+import Foundation
+
+enum Images: String {
+    case today = "today"
+    case history = "history"
+}
